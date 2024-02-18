b3869edfd9beab205e95d72c775d04ada6bb857e
Aryan Arabshahi <aryan.arabshahi@fotograf.de>
2024-02-18 01:20:47 +0100

    Adding the nested model detection.

--- portafilter/cli/test_model.py
+++ portafilter/cli/test_model.py
@@ -1,19 +1,54 @@
 from portafilter import Model, Ruleset
-from typing import Optional
 from portafilter.exceptions import ValidationError
+
+
+class Address(Model):
+    country: str
+    city: str
+    street: str
+    zip_code: str
+
+
+class Company(Model):
+    name: str
+    website: str
+    address: Address
 
 
 class User(Model):
     firstname: Ruleset('string')
+    lastname: Ruleset('string')
     email: Ruleset('string|nullable') = None
+    settings: Ruleset('list:dict')
+    company: Company
 
 
 def main():
 
     try:
-        user = User(firstname="test")
+        user = User(
+            firstname='Aryan',
+            lastname='Arabshahi',
+            settings=[
+                {
+                    'name': 'notifications',
+                    'value': True
+                }
+            ],
+            company={
+                'name': 'CodeWithCoffee',
+                'website': 'codewithcoffee.dev',
+                'address': {
+                    'country': 'Germany',
+                    'city': 'Berlin',
+                    'street': 'xxxx',
+                    'zip_code': 'xxxx'
+                }
+            }
+        )
+
         # print(user.email)
-        user.dict()
+        print(user.dict())
         # user = User(email='angel@gmail.com', test=123123)
         # print(user.email)
         # print(user.lastname)
--- portafilter/model.py
+++ portafilter/model.py
@@ -1,45 +1,20 @@
-from typing import List, Any
+from typing import List, Any, Tuple
 from portafilter.validator import Validator
 from portafilter.rules import Ruleset
+from portafilter.exceptions import ValidationError
 
 
 class ModelMetaclass(type):
 
     def __call__(cls, *args, **kwargs):
-        validation_rules = {}
-        data = {}
 
         instance = super().__call__(*args, **kwargs)
 
-        for attr_name, attr_annotation in cls.__annotations__.items():
-            # TODO:@@@@: Check this logic when setting the instance values
-            # TODO:@@@@: If there was no value assigmenet and no default value and no validation error for that field
-            # TODO:@@@@: Just raise this error
-            # TODO:@@@@: OR
-            # TODO:@@@@: 
-            # TODO:@@@@: Just simply add a required rule in the cases that the field
-            # TODO:@@@@: doesn't fill and no validation error with no default value!
-            # TODO:@@@@: after the data[attr_name] = getattr(instance, attr_name)
-            # if not hasattr(instance, attr_name) and attr_name not in kwargs:
-            #     raise TypeError(f"{cls.__name__}.__init__() missing required positional argument: '{attr_name}'")
+        data, validation_rules, nested_attributes = cls.__get_metadata(instance, kwargs)
 
-            if isinstance(attr_annotation, Ruleset):
-                if not attr_annotation.has_rule('present'):
-                    attr_annotation.add_rule('present')
-                validation_rules[attr_name] = attr_annotation
-
-            if attr_name in kwargs:
-                data[attr_name] = kwargs[attr_name]
-
-            elif hasattr(instance, attr_name):
-                data[attr_name] = getattr(instance, attr_name)
-
-            # else:
-            #     # The attribute is not in the input and has no default value.
-            #     raise AttributeError(f"The '{instance.__class__.__name__}.{attr_name}' has no value.")
-
-        # Validating the data.
-        Validator(data, validation_rules).validate()
+        validation_errors = cls.__get_validation_errors(data, validation_rules, nested_attributes)
+        if validation_errors:
+            raise ValidationError(errors=validation_errors)
 
         # Setting the class attribute
         for attribute, value in data.items():
@@ -47,28 +22,79 @@ class ModelMetaclass(type):
 
         return instance
 
+    @staticmethod
+    def __get_validation_errors(data: dict, validation_rules: dict, nested_attributes: list) -> dict:
+        validation_errors = {}
+
+        # Validating the data
+        validator = Validator(data, validation_rules)
+
+        if validator.fails():
+            validation_errors = validator.errors()
+
+        # Validating the nested models
+        for nested_attribute, nested_model in nested_attributes:
+            try:
+                data[nested_attribute] = nested_model(**data[nested_attribute])
+
+            except ValidationError as e:
+                for error_key, error_messages in e.errors().items():
+                    nested_key = f'{nested_attribute}.{error_key}'
+                    validation_errors[nested_key] = error_messages
+
+        return validation_errors
+
+    def __get_metadata(cls, instance, kwargs: dict) -> Tuple[dict, dict, list]:
+        data = {}
+        validation_rules = {}
+        nested_attributes = []
+
+        for attr_name, attr_annotation in cls.__annotations__.items():
+
+            if isinstance(attr_annotation, Ruleset):
+                if not attr_annotation.has_rule('present'):
+                    attr_annotation.add_rule('present')
+                validation_rules[attr_name] = attr_annotation
+
+            elif isinstance(attr_annotation, ModelMetaclass):
+                validation_rules[attr_name] = Ruleset('present')
+                nested_attributes.append((attr_name, attr_annotation))
+
+            if attr_name in kwargs:
+                data[attr_name] = kwargs[attr_name]
+
+            elif hasattr(instance, attr_name):
+                data[attr_name] = getattr(instance, attr_name)
+
+        return data, validation_rules, nested_attributes
+
 
 class Model(metaclass=ModelMetaclass):
 
     def __init__(self, *args, **kwargs):
-        attributes = self.__get_attributes()
+        attributes = self._get_attributes()
         for attribute_name, attribute_value in kwargs.items():
             if attribute_name not in attributes:
                 raise AttributeError(f"{self.__class__.__name__} object has no attribute '{attribute_name}'")
             setattr(self, attribute_name, attribute_value)
 
-    def __get_attributes(self) -> List:
+    def _get_attributes(self) -> List[str]:
         """Get the class attributes.
 
         Returns:
-            List
+            List[str]
         """
         attributes = []
         for attribute in self.__class__.__dict__:
             if not attribute.startswith("__"):
                 attributes.append(attribute)
-
         return list(set(attributes + list(self.__annotations__.keys())))
 
-    def dict():
-        pass
+    def dict(self) -> dict:
+        result = {}
+
+        for attribute in self._get_attributes():
+            attribute_value = getattr(self, attribute)
+            result[attribute] = attribute_value.dict() if isinstance(attribute_value, Model) else attribute_value
+
+        return result
